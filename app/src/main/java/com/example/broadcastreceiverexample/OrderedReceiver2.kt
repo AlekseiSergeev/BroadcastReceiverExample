57eb9834ecf093ac927efe6b2e0e8aa615fa41db
AlekseiSergeev <mrx45ru@gmail.com>
2021-12-02 19:31:37 +0500

    Add GoAsync + PendingResult

--- app/src/main/java/com/example/broadcastreceiverexample/OrderedReceiver2.kt
+++ app/src/main/java/com/example/broadcastreceiverexample/OrderedReceiver2.kt
@@ -3,29 +3,45 @@ package com.example.broadcastreceiverexample
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.SystemClock
 import android.widget.Toast
 
 class OrderedReceiver2: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        var resultCode = resultCode
-        var resultData = resultData
-        val resultExtras = getResultExtras(true)
-        var stringExtra = resultExtras.getString("keyString")
+        val handler = Handler()
 
-        resultCode++
-        stringExtra += "-> OR2"
+        val pendingResult = goAsync()
 
-        val toastText =
-            "OR2\n resultCode: $resultCode \n resultData: $resultData \n stringExtra: $stringExtra"
+        Thread(object : Runnable {
+            override fun run() {
 
-        Toast.makeText(context, toastText, Toast.LENGTH_LONG).show()
+                SystemClock.sleep(10 * 1000)
 
-        resultData = "OR2"
-        resultExtras.putString("keyString", stringExtra)
+                var resultCode = pendingResult.resultCode
+                var resultData = pendingResult.resultData
+                val resultExtras = pendingResult.getResultExtras(true)
+                var stringExtra = resultExtras.getString("keyString")
 
-        setResult(resultCode, resultData, resultExtras)
+                resultCode++
+                stringExtra += "-> OR2"
 
-       // abortBroadcast()
+                val toastText =
+                    "OR2 \n resultCode: $resultCode \n resultData: $resultData \n stringExtra: $stringExtra"
+
+                handler.post(object : Runnable {
+                    override fun run() {
+                        Toast.makeText(context, toastText, Toast.LENGTH_LONG).show()
+                    }
+                })
+
+                resultData = "OR2"
+                resultExtras.putString("keyString", stringExtra)
+
+                pendingResult.setResult(resultCode, resultData, resultExtras)
+                pendingResult.finish()
+            }
+        }).start()
     }
 }
